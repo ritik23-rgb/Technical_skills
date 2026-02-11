f70156883d6c86ddaf7fa46c75e664daf8f93fc6
Ritik Kumar <itik6465@gmail.com>
2026-02-11 10:44:32 +0530

    Add files via upload

---
+++ m.java
@@ -0,0 +1,21 @@
+//wap to read and print a user-defined matrix .
+import java.util.Scanner;
+class M {
+    public static void main(String[] ar) {
+        Scanner sc  = new Scanner(System.in);
+        int[][] a = new int[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
